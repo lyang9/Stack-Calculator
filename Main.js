2c0a2c3d2baa939c3b27863bd00a078afb0ee73f
Linda Yang <yanglinda24@gmail.com>
2019-04-02 19:12:14 -0400

    Clearing and swaping the redux store

--- Main.js
+++ Main.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet, Text, View, TouchableOpacity,
+} from 'react-native';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import { pressNum, enter, operation } from './modules';
+import {
+  pressNum, enter, operation, clear, swap, toggleNegative,
+} from './modules';
 import Button from './Button';
 
 const baseNumber = {
@@ -54,18 +58,27 @@ const App = ({
   pressNumWithDispatch,
   enterAction,
   operationAction,
+  clearAction,
+  swapAction,
+  toggleNegativeAction,
 }) => (
   <View style={styles.container}>
     <View style={styles.top}>
-      <Text style={styles.append}>{stack[2] || 0}</Text>
-      <Text style={styles.append}>{stack[1] || 0}</Text>
-      <Text style={styles[inputState]}>{stack[0] || 0}</Text>
+      <TouchableOpacity onPress={() => toggleNegativeAction(2)}>
+        <Text style={styles.append}>{stack[2] || 0}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => toggleNegativeAction(1)}>
+        <Text style={styles.append}>{stack[1] || 0}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => toggleNegativeAction(0)}>
+        <Text style={styles[inputState]}>{stack[0] || 0}</Text>
+      </TouchableOpacity>
     </View>
     <View style={styles.bottom}>
       <View style={styles.row}>
-        <Button text="clear" />
+        <Button text="clear" onPress={clearAction} />
         <Button text="pow" onPress={operationAction} />
-        <Button text="swap" />
+        <Button text="swap" onPress={swapAction} />
         <Button text="/" onPress={operationAction} />
       </View>
       <View style={styles.row}>
@@ -103,6 +116,9 @@ export default connect(
         pressNumWithDispatch: pressNum,
         enterAction: enter,
         operationAction: operation,
+        clearAction: clear,
+        swapAction: swap,
+        toggleNegativeAction: toggleNegative,
       },
       dispatch,
     ),
