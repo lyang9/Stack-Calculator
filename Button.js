a0b962e69b2ae75eb0d50baedcd08444e3c8aca8
Linda Yang <yanglinda24@gmail.com>
2019-04-02 23:10:00 -0400

    Animations in react native

--- Button.js
+++ Button.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import * as Animatable from 'react-native-animatable';
 
 const baseContainer = {
   alignItems: 'center',
@@ -31,13 +32,28 @@ const styles = StyleSheet.create({
   },
 });
 
-const Button = ({ text, special, onPress }) => (
-  <TouchableOpacity
-    onPress={() => onPress(text)}
-    style={special ? styles.specialContainer : styles.container}
-  >
-    <Text style={special ? styles.specialText : styles.text}>{text}</Text>
-  </TouchableOpacity>
-);
+class Button extends React.Component { 
+  render() {
+    const { text, special, onPress } = this.props;
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          this.text.rubberBand(400);
+          onPress(text);
+        }}
+        style={special ? styles.specialContainer : styles.container}
+      >
+        <Animatable.Text
+          ref={(ref) => {
+            this.text = ref;
+          }}
+          style={special ? styles.specialText : styles.text}
+        >
+          {text}
+        </Animatable.Text>
+      </TouchableOpacity>
+    );
+  }
+}
 
 export default Button;
